cc810e3f8dde022984377f073ea550ca8e2d933c
redgtz23 <james_maheux@hotmail.com>
2012-04-21 20:16:08 -0400

    Commit apres avoir downloadé les fichiers a dan

--- master.php
+++ master.php
@@ -11,7 +11,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="Content-Type" content="text/html" charset="iso-8859-1" xml:lang="fr" />
         <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css' />
+        <link href='http://fonts.googleapis.com/css?family=Poiret+One|Righteous' rel='stylesheet' type='text/css'/>
         <link rel="stylesheet" href="css/style.css"/>
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
         <title>WebConcept</title>
     <temp:contentPlaceHolder id="head">
@@ -153,7 +157,81 @@
             </div>
         </div>
     </div>
-    <div id="footer" class="siteWidth"></div>
+    <div class="floatingSeparator" style="height:50px;">
+    </div>
+    <div id="footer" class="siteWidth">
+        <div class="footerWrapper">
+            <div id="footerbox1" class="boxmargin">
+                <p class="titresFooter">PLAN DU SITE</p>
+                <div class="upperBorder">
+                    <div class="upperLeftBorder"></div>
+                    <div class="upperCenterLeftBorder"></div>
+                    <div class="upperCenterRightBorder"></div>
+                    <div class="upperRightBorder"></div>
+                </div>
+                <ul>
+                    <li>
+                        <a href="index.php"><p>Accueil</p></a>
+                    </li>
+                    <li>
+                        <a href="votre_page.php"><p>Votre page web</p></a>
+                    </li>
+                    <li>
+                        <a href="forfaits.php"><p>Forfaits</p></a>
+                    </li>
+                    <li>
+                        <a href="a_propos.php"><p>À propos</p></a>
+                    </li>
+                    <li>
+                        <a href="contactez_nous.php"><p>Contact</p></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div id="footerbox2" class="boxmargin">
+                <p class="titresFooter">GALERIE PHOTO</p>
+                <div class="upperBorder">
+                    <div class="upperLeftBorder"></div>
+                    <div class="upperCenterLeftBorder"></div>
+                    <div class="upperCenterRightBorder"></div>
+                    <div class="upperRightBorder"></div>
+                </div>
+                <div id="galeriePhoto">
+                    <div class="divPhoto"><img src="images/html5-css3.png"/></div>
+                    <div class="divPhoto"><img src="images/CSS3.png"/></div>
+                    <div class="divPhoto"><img src="images/graph1.png"/></div>
+                    <div class="divPhoto"><img src="images/compatible.png"/></div>
+                    <div class="divPhoto"><img src="images/W3C_logo.jpg"/></div>
+                    <div class="divPhoto"><img src="images/logo.png"/></div>
+                    <div class="clear" style="float:none;"></div>
+                </div>
+            </div>
+
+            <div id="footerbox3" class="boxmargin">
+                <p class="titresFooter">CONTACTEZ-NOUS</p>
+                <div class="upperBorder">
+                    <div class="upperLeftBorder"></div>
+                    <div class="upperCenterLeftBorder"></div>
+                    <div class="upperCenterRightBorder"></div>
+                    <div class="upperRightBorder"></div>
+                </div>
+                <form id="accueilForm" action="accueil_form_send.php" method="post">
+                    <div>
+                        <input type="text" name="name" id="formName" value="Votre nom"/><br/>
+                        <input type="email" name="email" id="formEmail" value="Votre courriel"/><br/>
+                        <textarea cols="21" rows="5" name="message" id="formMessage">Votre message</textarea><br/>
+                        <input type="submit" value="Envoyer" id="formButton"/>
+                    </div>
+                </form>
+            </div>
+            <div class="clear" style="float:none;"/>
+        </div>
+
+        <div id="social">
+            <div class="divSocialWidth"><span class="spancreation">Création web - <a href="#">Web Concept 2012</a></span></div>
+            <div class="divSocialWidth"><span class="spansocial">Suivez nous : </span><img src="images/facebook.png"/><img src="images/twitter.png"/><img src="images/linkedin.png"/><img class="imgBlogger" src="images/blogger.png" height="30" width="31"/></div>
+        </div>
+    </div>
     <script type="text/javascript">
         $(window).load(function(){
 
